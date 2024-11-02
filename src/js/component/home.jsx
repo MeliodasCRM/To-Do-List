fce06776dc93739a48bf7299b691208496000ebc
MeliodasCRM <Pabermb@gmail.com>
2024-11-02 19:20:46 +0000

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,51 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState("");
+
+	function handleChange(event) {
+		setNewTask(event.target.value);
+	}
+
+	function addTask() {
+		if (newTask) {
+			setTasks([...tasks, newTask]);
+			setNewTask("");
+		}
+	}
+
+	function deleteTask(index) {
+		const newTasks = tasks.filter((_, i) => i !== index);
+		setTasks(newTasks);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="todo-list">
+			<h1>To Do List</h1>
+			<div className="input-container">
+				<input
+					type="text"
+					placeholder="Add a new task"
+					value={newTask}
+					onChange={handleChange}
+					onKeyDown={(event) => {
+						if (event.key === "Enter") {
+							addTask();
+						}
+					}}
+				/>
+			</div>
+			<ul className="tasksToDo">
+				{tasks.map((elemento, index) => (
+					<li key={index} className="task-item">
+						{elemento}
+						<button className="delete-button" onClick={() => deleteTask(index)}>Eliminar</button>
+					</li>
+				))}
+			</ul>
+
 		</div>
 	);
 };
